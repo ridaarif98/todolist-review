4e88f4e88de44d9e830a4ecf0cdd1609a0cffb1a
ridaarif98 <ridaarif16@gmail.com>
2021-08-02 13:25:46 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 import 'lodash';
 import './style.css';
 import {
-    addList, stausCheck, editList, removeTodo, removeCompleted,
-  } from './status.js';
+  addList, stausCheck, editList, removeTodo, removeCompleted,
+} from './status.js';
   
-  const form = document.getElementById('addTodo');
+const form = document.getElementById('addTodo');
   
-  document.getElementById('showListItem').addEventListener('click', (e) => {
-    if (e.target.classList.contains('checkboX')) {
-      stausCheck(e);
-    }
-  });
+document.getElementById('showListItem').addEventListener('click', (e) => {
+  if (e.target.classList.contains('checkboX')) {
+    stausCheck(e);
+  }
+});
   
-  document.getElementById('showListItem').addEventListener('click', (e) => {
-    if (e.target.classList.contains('fa-ellipsis-v')) {
-      editList(e);
-    }
-  });
+document.getElementById('showListItem').addEventListener('click', (e) => {
+  if (e.target.classList.contains('fa-ellipsis-v')) {
+    editList(e);
+  }
+});
   
-  document.getElementById('showListItem').addEventListener('click', (e) => {
-    if (e.target.classList.contains('fa-trash-o')) {
-      removeTodo(e);
-    }
-  });
+document.getElementById('showListItem').addEventListener('click', (e) => {
+  if (e.target.classList.contains('fa-trash-o')) {
+    removeTodo(e);
+  }
+});
   
-  document.getElementById('clearButton').addEventListener('click', () => {
-    removeCompleted();
-  });
+document.getElementById('clearButton').addEventListener('click', () => {
+  removeCompleted();
+});
   
-  form.addEventListener('submit', (ev) => {
-    ev.preventDefault();
-    addList();
-  });
+form.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+  addList();
+});
