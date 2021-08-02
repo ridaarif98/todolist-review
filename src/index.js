1f9771719ba26e4650d6059ded91b007ea4b82b9
ridaarif98 <ridaarif16@gmail.com>
2021-08-02 13:28:00 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -3,31 +3,31 @@ import './style.css';
 import {
   addList, stausCheck, editList, removeTodo, removeCompleted,
 } from './status.js';
-  
+
 const form = document.getElementById('addTodo');
-  
+
 document.getElementById('showListItem').addEventListener('click', (e) => {
   if (e.target.classList.contains('checkboX')) {
     stausCheck(e);
   }
 });
-  
+
 document.getElementById('showListItem').addEventListener('click', (e) => {
   if (e.target.classList.contains('fa-ellipsis-v')) {
     editList(e);
   }
 });
-  
+
 document.getElementById('showListItem').addEventListener('click', (e) => {
   if (e.target.classList.contains('fa-trash-o')) {
     removeTodo(e);
   }
 });
-  
+
 document.getElementById('clearButton').addEventListener('click', () => {
   removeCompleted();
 });
-  
+
 form.addEventListener('submit', (ev) => {
   ev.preventDefault();
   addList();
