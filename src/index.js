10207df090ba9c233960964150044db8ea90632c
ridaarif98 <ridaarif16@gmail.com>
2021-08-02 13:09:25 +0200

    Import functions from status and sorting to index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
-import _ from 'lodash';
+import 'lodash';
 import './style.css';
+import {
+    addList, stausCheck, editList, removeTodo, removeCompleted,
+  } from './status.js';
+  
+  const form = document.getElementById('addTodo');
+  
+  document.getElementById('showListItem').addEventListener('click', (e) => {
+    if (e.target.classList.contains('checkboX')) {
+      stausCheck(e);
+    }
+  });
+  
+  document.getElementById('showListItem').addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-ellipsis-v')) {
+      editList(e);
+    }
+  });
+  
+  document.getElementById('showListItem').addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-trash-o')) {
+      removeTodo(e);
+    }
+  });
+  
+  document.getElementById('clearButton').addEventListener('click', () => {
+    removeCompleted();
+  });
+  
+  form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    addList();
+  });
